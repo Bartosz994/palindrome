9f0da1a82ee7116dcd4eeffae87e700e3d85c10e
Bartosz94 <bartosz.luczak@cense.com>
2025-06-17 16:10:06 +0200

    Finish working and refactored palindrome method

--- index.js
+++ index.js
@@ -10,9 +10,12 @@ function Phrase(content) {
 
   // Returns content processed for palindrome testing.
   this.processedContent = function processedContent() {
-    return this.content.toLowerCase();
+    return this.letters().toLowerCase();
   }
 
+  this.letters = function letters() {
+   return Array.from(this.content).filter(c => c.match(/[a-z]/i)).join("");
+  }
   // Returns true if the phrase is a palindrome, false otherwise.
   this.palindrome = function palindrome() {
     return this.processedContent() === this.processedContent().reverse();
